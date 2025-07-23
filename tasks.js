e37aca6ef1e5c24652db280b7371b21a6c104b02
morgenlys <92630572+morgenlys@users.noreply.github.com>
2025-07-24 01:18:12 +0200

    Update tasks.js

--- tasks.js
+++ tasks.js
@@ -1,32 +1,62 @@
 const tasks = [
   {
-    type: "drag-platt-to-de",
-    question: "Wi gahn na Huus.",
-    correct: ["Wir", "gehen", "nach", "Hause"],
-    gloss: ["Wir", "gehen", "nach", "Hause"]
+    "type": "drag-platt-to-de",
+    "question": "Gur Dai",
+    "correct": ["Guten", "Tag"],
+    "gloss": ["Guten", "Tag"]
   },
   {
-    type: "drag-platt-to-de",
-    question: "He is mien Brör.",
-    correct: ["Er", "ist", "mein", "Bruder"],
-    gloss: ["Er", "ist", "mein", "Bruder"]
+    "type": "drag-platt-to-de",
+    "question": "Hur gair't wat?",
+    "correct": ["Wie", "geht", "es"],
+    "gloss": ["wie", "geht", "es"]
   },
   {
-    type: "drag-platt-to-de",
-    question: "Dat is good.",
-    correct: ["Das", "ist", "gut"],
-    gloss: ["Das", "ist", "gut"]
+    "type": "drag-platt-to-de",
+    "question": "Fuul Dank",
+    "correct": ["Vielen", "Dank"],
+    "gloss": ["Vielen", "Dank"]
   },
   {
-    type: "drag-platt-to-de",
-    question: "Ik hebb Hunger.",
-    correct: ["Ich", "habe", "Hunger"],
-    gloss: ["Ich", "habe", "Hunger"]
+    "type": "drag-platt-to-de",
+    "question": "Sünhair",
+    "correct": ["Gesundheit"],
+    "gloss": ["Gesundheit"]
   },
   {
-    type: "drag-platt-to-de",
-    question: "Se speelt mit de Kat.",
-    correct: ["Sie", "spielt", "mit", "der", "Katze"],
-    gloss: ["Sie", "spielt", "mit", "der", "Katze"]
+    "type": "drag-platt-to-de",
+    "question": "Jaa",
+    "correct": ["Ja"],
+    "gloss": ["Ja"]
+  },
+  {
+    "type": "drag-platt-to-de",
+    "question": "Naan",
+    "correct": ["Nein"],
+    "gloss": ["Nein"]
+  },
+  {
+    "type": "drag-platt-to-de",
+    "question": "Ik sen litj",
+    "correct": ["Ich", "bin", "klein"],
+    "gloss": ["Ich", "bin", "klein"]
+  },
+  {
+    "type": "drag-platt-to-de",
+    "question": "Dü best gurt",
+    "correct": ["Du", "bist", "groß"],
+    "gloss": ["Du", "bist", "groß"]
+  },
+  {
+    "type": "drag-platt-to-de",
+    "question": "Hi es net",
+    "correct": ["Er", "ist", "nett"],
+    "gloss": ["Er", "ist", "nett"]
+  },
+  {
+    "type": "drag-platt-to-de",
+    "question": "Gur Nacht",
+    "correct": ["Gute", "Nacht"],
+    "gloss": ["Gute", "Nacht"]
   }
 ];
